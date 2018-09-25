38c07879f85bbb4aae2a748f7b987c44c14a8808
FelixGrasser <43424966+FelixGrasser@users.noreply.github.com>
2018-09-25 18:34:35 -0500

    Add files via upload

---
+++ secretproject.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class secretproject
+{
+public static void main(String[] arg)
+{
+    // Project is not yet completed, the purpose of this project is to create a database where items can be searched.
+    // Another purpose of this project is to check prices and the current inventory of these items as well.
+   
+   // List of fruit avaliable to purchase.
+   
+     double apple;
+     double pear;
+     double melon;
+     double pineapple;
+     double strawberries;
+     double oranges;
+     double rasberries;
+     double total;
+
+   // Price of the fruit avaliable to purchase 
+
+     apple = 2.99;
+     pear = 4.99;
+     melon = 5.99;
+     pineapple = 6.99;
+     strawberries = 3.99;
+     oranges = 4.99;
+     rasberries = 5.99;
+
+     total = apple, pear, melon, pineapple, strawberries, oranges, rasberries;
+     
+     // Prints the name and the price of the fruit.
+     
+     System.out.print("\nApple: $" + apple);
+     System.out.print("\nPear: $" + pear);
+     System.out.print("\nmelon: $" + melon);
+     System.out.print("\npineapple: $" + pineapple);
+     System.out.print("\nstrawberries: $" + strawberries);
+     System.out.print("\noranges: $" + oranges);
+     System.out.print("\nrasberries: $" + rasberries);
+     
+     
+     // Input the fruit that you are looking for to see the price.
+     
+     Scanner input = new Scanner(System.in);
+
+     System.out.print("Please enter the fruit you are looking for");
+     total = input.nextDouble();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
